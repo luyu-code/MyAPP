67a46165f7cf8b20bcfef57455000e36f1e57d31
Your Name <Guang06611@163.com>
2021-10-14 21:53:18 +0800

    2021.10.14.第三次提交 测试

--- app/src/main/java/com/example/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapp/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.myapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.myapp.R;
 
@@ -12,5 +13,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(this, "登录", Toast.LENGTH_SHORT).show();
     }
 }
